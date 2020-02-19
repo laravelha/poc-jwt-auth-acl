73093597b43962cbc5e2ff07adde5cfad8e9e01f
Pedro Augusto <pams.pedro@gmail.com>
2020-02-19 18:53:01 -0300

    feat(preset): apply ha-api preset

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'bindings',
+            'cors',
         ],
     ];
 
@@ -61,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
