2fe13971b3972ac3a49882c74c5c306e309e4d0b
Pedro Augusto <pams.pedro@gmail.com>
2020-02-19 19:21:31 -0300

    improvement(jwt): set auth config to jwt as default

--- config/auth.php
+++ config/auth.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laravelha\Auth\Models\User;
+
 return [
 
     /*
@@ -14,7 +16,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -42,9 +44,8 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt',
             'provider' => 'users',
-            'hash' => false,
         ],
     ],
 
@@ -68,13 +69,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => User::class,
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
